9578bbfe385d7122b71d301637f68664d394bcce
Cody Crawford <Cody.Crawford1010@gmail.com>
2022-04-18 11:59:20 -0500

    completedExercise

---
+++ bonus.js
@@ -0,0 +1,24 @@
+// 3a
+import {useState} from 'react';
+
+// 3b
+function Bonus(){
+    // 3d
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    // 3h
+    const [letterCount, setLetterCount] = useState(0);
+
+    // 3e
+    return(
+        <div>
+           <h1>{letters[letterCount]}</h1>
+           <button onClick={() => (letterCount === letters.length - 1) ? setLetterCount(0) : setLetterCount(letterCount + 1)}>NEXT LETTER</button>
+           {/* OR */}
+           {/* <button onClick={() => setLetterCount(letterCount + 1)}>NEXT LETTER</button> */}
+        </div>
+    );
+
+}
+
+// 3c
